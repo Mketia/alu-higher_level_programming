8e775390303e262b40e81ce2e2b20b2565460a37
root <k.mukakalis1@alustudent.com>
2024-04-13 05:57:55 -0700

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""Script that lists all cities from the database hbtn_0c_4_usa"""
-import MySQLdb
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
+    # Get MySQL credentials and search name from command-line arguments
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Connect to MySQL server
     c = db.cursor()
-    c.execute("""SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+
+    # Execute the SQL query to retrieve all states
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                  FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`""")
+                ORDER BY `c`.`id`")
+
+    # Fetch all rows and print the states
     [print(city) for city in c.fetchall()]
