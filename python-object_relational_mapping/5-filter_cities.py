7a30c99237eac42b3901bd1f9b7c987cd2184800
root <k.mukakalis1@alustudent.com>
2024-04-13 06:00:04 -0700

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-"""Script that takes in the name of a state as an argument and lists all cities of that state using the database hbtn_0c_4_usa"""
+
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Get MySQL credentials and state name from command-line arguments
+    # and Connect to MySQL server
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
+
+    # Execute the SQL query to retrieve cities in the specified state
+    query = ("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
+    c.execute(query)
+
+    # Fetch all rows and filter cities by the specified state
+    # and Print the cities separated by commas
     print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
