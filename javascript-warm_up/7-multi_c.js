42bbe84f421b1f69dc5285b1e14eacbccc845de3
@Mketia <k.mukakalis1@alustudent.com>
2024-06-07 06:34:07 +0000

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const x= parseInt(process.argv[2]);
+
+const args = process.argv;
+const x = parseInt(args[2]);
+
 if (isNaN(x)) {
-	console.log('Missing number of occurences');
-}
-else{
-	for (let i = 0; i<x; i++){
-		console.log('C is fun');
-	}
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
