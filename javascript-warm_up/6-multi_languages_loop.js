b0b8e37ec2a813df78e06f26f366e1f3411189f0
@Mketia <k.mukakalis1@alustudent.com>
2024-06-07 06:43:22 +0000

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const msg= ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while (i < msg.length) {
-	console.log(msg[i]);
-	i++;
+
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
 }
