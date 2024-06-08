c78bef62087187d5c41e1aa7ba0e5339f41e3b45
@Mketia <k.mukakalis1@alustudent.com>
2024-06-08 11:04:39 +0000

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 
-const input = process.argv[2];
-const isNumber = !isNaN(parseInt(input, 10));
-
-if (isNumber) {
-  console.log(`My number: ${parseInt(input, 10)}`);
-} else {
-  console.log('Not a number');
-=======
 const input = process.argv[2];
 const isNumber = !isNaN(parseInt(input));
 
 if (isNumber) {
-	console.log(`My number: ${parseInt(input)}`);
-}
-else{
-	console.log('Not a number');
->>>>>>> 344b76431a06fdf2c1adb686aab71ca27008188e
+  console.log(`My number: ${parseInt(input)}`);
+} else {
+  console.log('Not a number');
 }
