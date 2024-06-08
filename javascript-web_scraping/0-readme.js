56ccd12f7e2f710da0f55583d667693f99de5300
@Mketia <k.mukakalis1@alustudent.com>
2024-06-08 09:58:24 +0000

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
-const fs= require('fs');
 
-function readFile (filePath){
-	fs.readFile(filePath, 'utf-8', (err,data) => {
-		if(err){
-			console.error(`An error has occured: ${err}`);
-		}else{
-			console.log(data);
-		}
-	});
+const fs = require('fs');
+
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`An error has occurred: ${err}`);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
-if (process.argv.length >2) {
-	const filePath = process.argv[2];
-	readFile(filePath);
-} else{
-	console.log('Provide the file path.');
+if (process.argv.length > 2) {
+  const filePath = process.argv[2];
+  readFile(filePath);
+} else {
+  console.log('Provide the file path.');
 }
